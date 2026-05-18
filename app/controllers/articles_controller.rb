7be438ebf93c3260edde38ed3ec9785d8caa7fbc
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-05-18 14:21:52 -0300

    passando o metodo find

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     # se eu não passa o article no controller ele não vai
   end
+
+  def show 
+   @article = Article.find(params[:id])  
+  end
 end
