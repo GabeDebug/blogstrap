e77359217b572a9eea6a5ae343232f0fcf3b3173
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-05-19 12:22:20 -0300

    mostrando article usando link_to

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  resources :articles
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
